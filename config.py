613193e0fafe6bfe27d51c3a5299679600b69255
Eduardo Echeverria <echevemaster@gmail.com>
2014-11-22 02:45:16 -0430

    First commit of config file

---
+++ config.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import os
+import datetime
+# Belonging to the path of  sqlite development database.
+basedir = os.path.abspath(os.path.dirname(__file__))
+engine = 'sqlite'  # sqlite, Mysql, postgres and so on.
+db_name = 've-api.db'
+db_user = ''
+db_password = ''
+db_server = 'localhost'  # Usually, localhost
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SQLALCHEMY_ECHO = True
+    
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = '{0}://{1}:{2}@{3}/{4}'.format(engine,
+                                                             db_user,
+                                                             db_password,
+                                                             db_server,
+                                                             db_name)
+    DATABASE_CONNECT_OPTIONS = {}
+    SECRET_KEY = 'key'
+    CSRF_SESSION_KEY = "somethingimpossibletoguess"
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = engine + ':///' + os.path.join(basedir,
+                                                             db_name)
+    DATABASE_CONNECT_OPTIONS = {}
+    SQLALCHEMY_ECHO = True
+    SECRET_KEY = 'This string will be replaced with a proper key in production'
+    CSRF_SESSION_KEY = "somethingimpossibletoguess"
+
+
+class TestingConfig(Config):
+    TESTING = True
